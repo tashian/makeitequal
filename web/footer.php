0eda1775981eb2f6f4294f2cdf13607dad9f467d
tashian <carl@tashian.com>
2017-09-09 07:48:40 -0700

    Cleanup footer and contact pages

--- web/footer.php
+++ web/footer.php
@@ -2,25 +2,15 @@
 	<div class="container_12">  
 
   	<div class="grid_4">
-  		<h2>Email list</h2>
-  		<p class="caption">Subscribe to our low-volume email list for updates from the Flag of Equal Marriage.</p> 
-  			<form class="listsignup" action="http://makeitequal.createsend.com/t/r/s/yhyuiy/" method="post">
-			<label>Enter your e-mail:</label>
-		    <input type="text" name="cm-yhyuiy-yhyuiy" id="yhyuiy-yhyuiy" placeholder="Enter your e-mail" class="email cleardefault" />
-			<label>US Zip Code (optional):</label>
-    		<input type="text" name="cm-f-butld" placeholder="US Zip Code (optional)" class="zipcode" id="USZipCode" />
-
-    		<input type="submit" value="Subscribe" class="long standard button shadow" />
-     		</form>
-  	</div>
-  	
-  	<div class="grid_4">
   		<h2>Keep in Touch</h2>
   		<p class="caption">Connect on Facebook and Twitter.</p>
   		<p><a href="https://www.facebook.com/makeitequal" class="long facebook button shadow"><img src="/global/images/share-fb@2x.png" style="width:10px; height: 19px;"> <span>Like the flag on</span> Facebook</a></p>
   		<p><a href="http://twitter.com/makeitequal" class="long twitter button shadow"><img src="/global/images/share-tw@2x.png" style="width:18px; height: 16px;"> <span>Follow the flag on</span> Twitter</a></p>
   	</div>
 
+  	<div class="grid_4">
+  	</div>
+  	
   	<div class="grid_4">
   		<h2>Dashboard Widget</h2>
   		<p class="caption"><img src="/global/images/widget.png"  srcset="/global/images/widget.png 1x, /global/images/widget@2x.png 2x" style="float: left; width: 87px; height: 90px;" class="inline" alt="Dashboard Widget Icon"> If you're on Mac OS X, <a href="/dashboard/download/MarriageFlag.wdgt.zip">download the Flag of Equal Marriage Dashboard widget</a> to see the latest flag, along with same-sex marriage news headlines from our friends at <a href="http://www.freedomtomarry.org/">Freedom to Marry</a>.</p>
